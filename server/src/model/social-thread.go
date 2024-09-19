098bfadc68273eb36a06dc6c8f63062cff46f807
bmbmbmbmbmbm <mckjamin.benenzie@gmail.com>
2024-09-19 20:26:12 +0100

    apparently go likes column formatting

--- server/src/model/social-thread.go
+++ server/src/model/social-thread.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type thread struct {
-	id string `json:"id"`
-	title string `json:"title"` //optional
-	content string `json:"name"`
+	id      string    `json:"id"`
+	title   string    `json:"title"` //optional
+	content string    `json:"name"`
 	created time.Time `json:"name"`
 }
